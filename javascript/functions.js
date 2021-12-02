f60b0596312a65a9b748069a4114ad330e69c0ca
Anita Angelidou <anitawebdesigns@gmail.com>
2021-12-02 13:53:33 +0200

    fixed: js in glutenf div to display only products of glutenf category

---
+++ javascript/functions.js
@@ -0,0 +1,116 @@
+const categProducts = [
+  {
+    category: "cakes",
+    name: "Chocolate Truffle Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2015/11/chocolate_truffle_cake.png",
+    alt: "image of a chocolate truffle cake",
+    class: "discount",
+  },
+
+  {
+    category: "cakes",
+    name: "Almond fudge cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2015/11/300256-Jamocha-Almond-Fudge_B_small.png",
+    alt: "iimage of an almond fudge cake",
+    class: "low",
+  },
+
+  {
+    category: "cakes",
+    name: "Carrot Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2015/11/300188-Carrot_B_small.png",
+    alt: "image of a carrot cake",
+    class: "",
+  },
+
+  {
+    category: "cakes",
+    name: "Italian Rum Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2017/06/italian_rum_cake.png",
+    alt: "image of italian rum cake",
+    class: "out",
+  },
+
+  {
+    category: "cakes",
+    name: "Everything Chocolate Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2021/03/300606-Everything-Chocolate_small-1.png",
+    alt: "image of an everything chocolate cake",
+    class: "",
+  },
+
+  {
+    category: "cakes",
+    name: "Milk Chocolate Caramel Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2015/11/300273-Milk-Chocolate-Caramel-Cake_small.png",
+    alt: "image of a milk chocolate caramel cake",
+    class: "",
+  },
+
+  {
+    category: "cakes",
+    name: "Apple Crumble Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2021/06/300771-Apple-Crumble_B_small.png",
+    alt: "image of an apple crumble cake",
+    class: "discount",
+  },
+
+  {
+    category: "cakes",
+    name: "Caramel Crunch Cake",
+    imageURI:
+      "https://www.laroccacakes.com/wp-content/uploads/2015/11/300317_B-small.png",
+    alt: "image of a caramel crunch cake",
+    class: "",
+  },
+
+
+  {
+    category: "pastries",
+    name: "Black Forest",
+    imageURI:
+      "https://www.papanaretos.gr/wp-content/uploads/2014/04/2.Pasta_BLACK-FOREST.png",
+    alt: "image of a black forest pastry",
+    class: "",
+  },
+ 
+  
+
+
+  {
+    category: "glutenf",
+    name: "Gluten free turtle cheesecake",
+    imageURI:
+      "https://images.squarespace-cdn.com/content/v1/538500e4e4b0fa9e95efc7b9/1474392514481-YP6YDWSILDGN2OL8VUDD/adj_GF_TurtleCC.png?format=500w",
+    alt: "image of a gluten free turtle cheesecake",
+    class: "discount",
+  },
+
+  {
+    category: "glutenf",
+    name: "Gluten free flourless Chocolate Cake",
+    imageURI:
+      "https://www.cakesonlineaustralia.com.au/wp-content/uploads/2020/01/FlourlessChocMud.png",
+    alt: "image of a gluten free flourless chocolate cake",
+    class: "",
+  },
+
+  {
+    category: "glutenf",
+    name: "Gluten free Caramel Cake",
+    imageURI:
+      "https://www.chriskitchen.com.au/wp-content/uploads/2020/08/homepage-header-cake.png",
+    alt: "image of a gluten free caramel cake",
+    class: "",
+  },
+
+
+
+];
